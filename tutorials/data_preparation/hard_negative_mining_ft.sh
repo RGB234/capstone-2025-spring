e4676a552353d29e3ade72e1af4ee608ce7fd914
RGB234 <fehs0611@gmail.com>
2025-10-31 13:02:24 +0900

    change positive sample counts per a query from 5 to 20

--- tutorials/data_preparation/hard_negative_mining_ft.sh
+++ tutorials/data_preparation/hard_negative_mining_ft.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 #SBATCH -J bge-m3
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-gpu=6
-#SBATCH --mem-per-gpu=20G
-#SBATCH -w aurora-g3
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=32G
+#SBATCH -w aurora-g8
 #SBATCH -p batch_ugrad
 #SBATCH -t 1-0
 #SBATCH -o logs/slurm-%A.out
@@ -12,10 +12,10 @@ pwd
 
 python hard_negative_mining.py \
 --embedder_name_or_path BAAI/bge-m3 \
---input_file /data2/local_datasets/encoder/data/incidents_ft.jsonl \
---output_file /data2/local_datasets/encoder/data/incidents_ft_minedHN.jsonl \
---range_for_sampling 2-300 \
---negative_number 32 \
+--input_file /data2/local_datasets/encoder/dataset/incidents_ft.jsonl \
+--output_file /data2/local_datasets/encoder/dataset/incidents_ft_minedHN.jsonl \
+--range_for_sampling 10-210 \
+--negative_number 15 \
 --use_gpu_for_searching 
 
 exit 0
